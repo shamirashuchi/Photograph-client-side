ce43ed56701df47322bb841cee2382293f56d143
shamirashuchi <shamirashuchi@gmail.com>
2023-06-08 11:29:56 +0600

    firebase-added

--- src/Pages/Home/Login/Login.jsx
+++ src/Pages/Home/Login/Login.jsx
@@ -1,10 +1,68 @@
-import React from 'react';
-
+import React, { useContext, useEffect,useState } from 'react';
+import { AuthContext } from '../../Providers/Authprovider';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
+import SocialLogin from '../../Shared/SocialLogin/SocialLogin';
 const Login = () => {
+    const {signIn} = useContext(AuthContext);
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const from = location.state?.from?.pathname || "/";
+    const handleLogin = event =>{
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email,password);
+        signIn(email,password)
+        .then(result =>{
+            const user = result.user;
+            console.log(user);
+            Swal.fire({
+                title: 'User Login Successful.',
+                showClass: {
+                    popup: 'animate__animated animate__fadeInDown'
+                },
+                hideClass: {
+                    popup: 'animate__animated animate__fadeOutUp'
+                }
+            });
+            navigate(from, { replace: true });
+        })
+    }
     return (
-        <div>
-            <h2>This is login page</h2>
+        <>
+            <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="text-center md:w-1/2 lg:text-left">
+                <h1 className="text-5xl font-bold">Login now!</h1>
+                <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                </div>
+                <div className="card lg:w-1/3 max-w-sm shadow-2xl bg-base-100">
+                <form onSubmit={handleLogin} className="card-body">
+                    <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Email</span>
+                    </label>
+                    <input type="email" name="email" placeholder="email" className="input input-bordered w-80"  />
+                    </div>
+                    <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Password</span>
+                    </label>
+                    <input type="password" name="password" placeholder="password" className="input input-bordered w-80" />
+                    </div>
+                    <div className="form-control mt-6">
+                    <input disabled={false} className="btn btn-primary w-80" type="submit" value="Login" />
+                    </div>
+                </form>
+                <p><small>New Here?<Link to="/signup">create an account</Link></small></p>
+                <SocialLogin></SocialLogin>
+                </div>
+            </div>
         </div>
+        </>
     );
 };
 
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDQKWPROW461WjXlF9OMDXHv_rdNkP_K0o",
+  authDomain: "photograph-ad994.firebaseapp.com",
+  projectId: "photograph-ad994",
+  storageBucket: "photograph-ad994.appspot.com",
+  messagingSenderId: "679641932835",
+  appId: "1:679641932835:web:5a9760392aa4760b923d96"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
