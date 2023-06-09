3da5be0b4689e86b2ac0b2afe727bd9898b3d451
shamirashuchi <shamirashuchi@gmail.com>
2023-06-09 14:58:03 +0600

    verify token

--- src/Hooks/useSelecteddata.jsx
+++ src/Hooks/useSelecteddata.jsx
@@ -4,20 +4,20 @@ import { AuthContext } from '../Providers/Authprovider';
 
 const useSelecteddata = () => {
   const { user, loading } = useContext(AuthContext);
-
+  const token = localStorage.getItem('access-token');
   const { refetch, data: classdata = [] } = useQuery({
     queryKey: ['selects', user?.email],
-    enabled: !!user, // Only enable the query if the user object is available
+    enabled: !!user, 
     queryFn: async () => {
-      const res = await fetch(`http://localhost:2000/selects?email=${user?.email}`);
+      const res = await fetch(`http://localhost:2000/selects?email=${user?.email}`,{ headers: {
+                authorization: `bearer ${token}`
+             }});
       return res.json();
     },
   });
 
   useEffect(() => {
     if (!loading && !user) {
-      // Handle the case when the user is not available during the initial load
-      // You can redirect to a login page or show an appropriate message
       console.log('User not available');
     }
   }, [loading, user]);
