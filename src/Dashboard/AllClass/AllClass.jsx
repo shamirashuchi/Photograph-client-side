20c357504ef6706c01f1db1525661390ac9fe25c
shamirashuchi <shamirashuchi@gmail.com>
2023-06-09 11:56:37 +0600

    data come to all user and all class page

--- src/Dashboard/AllClass/AllClass.jsx
+++ src/Dashboard/AllClass/AllClass.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import { useQuery } from "@tanstack/react-query";
 const AllClass = () => {
+    const { data: classes = [], refetch } = useQuery(['class'], async () => {
+        const res = await fetch('http://localhost:2000/class')
+        return res.json();
+    })
     return (
         <div>
-            <h2>All Class</h2>
+            <h2>{classes.length}</h2>
         </div>
     );
 };
--- src/Dashboard/AllUser/AllUser.jsx
+++ src/Dashboard/AllUser/AllUser.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import { useQuery } from "@tanstack/react-query";
 const AllUser = () => {
+    const { data: users = [], refetch } = useQuery(['users'], async () => {
+        const res = await fetch('http://localhost:2000/users')
+        return res.json();
+    })
     return (
         <div>
-            <h2>All user</h2>
+            <h2>{users.length}</h2>
         </div>
     );
 };
