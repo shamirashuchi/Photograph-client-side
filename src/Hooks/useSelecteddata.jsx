c0d1a9d921e41dfb53f73828c2140fc93e2ba751
shamirashuchi <shamirashuchi@gmail.com>
2023-06-09 15:25:19 +0600

    use axios secure

--- src/Hooks/useSelecteddata.jsx
+++ src/Hooks/useSelecteddata.jsx
@@ -1,18 +1,18 @@
 import React, { useContext, useEffect } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { AuthContext } from '../Providers/Authprovider';
+import useAxiosSecure from './useAxiosSecure';
 
 const useSelecteddata = () => {
   const { user, loading } = useContext(AuthContext);
+  const [axiosSecure] = useAxiosSecure();
   const token = localStorage.getItem('access-token');
   const { refetch, data: classdata = [] } = useQuery({
     queryKey: ['selects', user?.email],
-    enabled: !!user, 
+    enabled: !!user,
     queryFn: async () => {
-      const res = await fetch(`http://localhost:2000/selects?email=${user?.email}`,{ headers: {
-                authorization: `bearer ${token}`
-             }});
-      return res.json();
+      const res = await axiosSecure.get(`/selects?email=${user?.email}`);
+      return res.data;
     },
   });
 
@@ -21,8 +21,9 @@ const useSelecteddata = () => {
       console.log('User not available');
     }
   }, [loading, user]);
-console.log(classdata);
-  return [classdata,refetch];
+
+  console.log(classdata);
+  return [classdata, refetch];
 };
 
 export default useSelecteddata;
